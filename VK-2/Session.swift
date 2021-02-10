35675083e7389320959dc0c87dbe136abda941ba
YuliaShlaeva <shlaeva@gmail.com>
2021-02-10 16:37:17 +0300

    singletone Session

---
+++ VK-2/Session.swift
@@ -0,0 +1,18 @@
+//
+//  Session.swift
+//  VK-2
+//
+//  Created by Julie on 10.02.2021.
+//
+
+import UIKit
+
+class Session {
+    
+    static let shared = Session()
+    private init() { }
+    
+    var token: String = ""
+    var userid: Int = 0
+    
+}
